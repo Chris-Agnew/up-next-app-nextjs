976a600d3e9a13bca73428ead03a9d4bffb8e643
Chris <chris@chris-agnew.com>
2021-08-07 17:01:09 -0400

    ADD: Navbar component and styles

---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFireAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTicketAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTv } from '@fortawesome/free-solid-svg-icons'
+const Navbar = () => {
+	return (
+		<nav>
+			<div className="flex justify-between items-center pt-1 bg-gray-900 text-gray-200 h-20">
+				<div className="ml-5">
+					<div>Up-Next.App</div>
+				</div>
+				<div className="cursor-pointer ">
+					<ul className="flex">
+						<li className="px-2 hover:animate-bounce hover:text-blue-500">
+							<FontAwesomeIcon  icon={faFireAlt}/> Popular Shows
+						</li>
+						<li className="px-2 hover:animate-bounce hover:text-blue-500 ">
+						<FontAwesomeIcon  icon={faTicketAlt}/>	New Shows						</li>
+						<li className="px-2 hover:animate-bounce hover:text-blue-500">
+							<FontAwesomeIcon  icon={faTv}/> Coming Soon
+						</li>
+						<li className="px-2 hover:animate-bounce hover:text-blue-500">
+						<FontAwesomeIcon  icon={faUserAlt}/>	Account
+						</li>
+					</ul>
+					
+				</div>
+				<div>
+				<a href="#" className="px-2 bg-blue-700 rounded-lg p-2 mr-5"> Login/Sign Up</a>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
